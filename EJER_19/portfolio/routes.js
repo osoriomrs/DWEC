4da0d89dccc5dc59b273678d29b860a5c922bd8a
Sergio Osorio <156062683+osoriomrs@users.noreply.github.com>
2026-05-06 11:23:25 +0200

    Delete EJER_19 directory

--- EJER_19/portfolio/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { portfolio } from "./controller.js";
-
-const router = express.Router();
-
-router.get("/:username", portfolio);
-
-export default router;
--- EJER_19/utils/md5.js
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export const md5 = (text) => {
-  return crypto.createHash("md5").update(text).digest("hex");
-};
